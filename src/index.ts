5ec16440eac2943b5892e4a79b56b07f480e6834
Erick Devv <me440144@uaeh.edu.mx>
2023-10-14 16:26:26 -0600

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Express, Request, Response } from "express";
+
+import dotenv from "dotenv";
+
+import express from "express";
+
+dotenv.config();
+
+const app: Express = express()
+
+const port = process.env.PORT || 5050;
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Express + Cronos");
+})
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}!`);
+})
